4b261a04534ba902db256312bacb5a58869509b4
Andrew G <andrew.samoilov@gmail.com>
2011-06-16 00:15:29 +0400

    edited some styles

--- main/views/users_interface/footer.php
+++ main/views/users_interface/footer.php
@@ -1,10 +1,10 @@
 <div class="footer">
 	<div class="container_16">
 		<div class="footer_left">
-			<p> &copy; Copyright 2011 All Rights Reserved by <a href="http://nsp-don.ru/">nsp-don.ru</a></p>
+			<p> Все права защищены &copy; 2011 <a href="http://nsp-don.ru/">nsp-don.ru</a></p>
 		</div>
 		<div class="footer_right">
-			<p>Designed and Developed by <a href="http://www.realitygroup.ru" target="_blank">realitygroup.ru</a></p>
+			<p>Разработка веб-сайта <a href="http://www.realitygroup.ru" target="_blank">ТО Реальность</a></p>
 		</div>
 	</div>
 </div>
